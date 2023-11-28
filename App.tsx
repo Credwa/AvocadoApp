71c92ab72e35dc4362da78f0bea8d8b714d0b00b
Craig Edwards <craigroe7@gmail.com>
2023-11-28 15:19:53 -0400

    Add sentry

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+// slug avocado-zn
+
+Sentry.init({
+  dsn: "https://d50d3a482469ae4a1ccb8e142683384e@o4506305008173056.ingest.sentry.io/4506305022263296",
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
   return (
